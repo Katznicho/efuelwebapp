4787509c2332705f4adc4d0b595e5ad36f51b19e
katznicho <katznicho@gmail.com>
2023-08-13 16:16:08 +0300

    end-no-edit

--- views/home.php
+++ views/home.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user'])) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Credit Plus Dashboard</title>
+	<title>E Fuel Dashboard</title>
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<!-- Font Awesome -->
